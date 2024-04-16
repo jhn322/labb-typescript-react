4c9206cbd3993f6bf0065a5eea7302ab329dff8a
Johan Söderlund <johan.soderlund96@gmail.com>
2024-04-17 01:24:51 +0200

    edited main, basic styling, array for winning conditions, state, interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface GameState {
+  player: string | null;
 }
 
-export default App
+const App = () => {
+  const [gameState, setGameState] = useState<GameState[]>(
+    Array(9).fill({ player: null })
+  );
+  const [currentPlayer, setCurrentPlayer] = useState<string>("X");
+
+  const winConditions: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  return (
+    <div>
+      <h1>TicTacToe</h1>
+    </div>
+  );
+};
+
+export default App;
